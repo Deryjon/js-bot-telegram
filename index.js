d21df6b746ee5ec310eaeb6e19285ddfc5a2be9b
Dery Jon <yusupoviskandar1005@gmail.com>
2024-06-18 21:48:39 +0500

    feat: add menu commands (2024-06-18, 21:48)

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-require('dotenv').config()
-const {Bot, GrammyError, HttpError} = require('grammy');
+require("dotenv").config();
+const { Bot, GrammyError, HttpError } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
-bot.command('start', async (ctx) => {
-    await ctx.reply('Привет! Я бот.')
+bot.api.setMyCommands([
+    { command: "start", description: "Start bot" },
+    { command: "hello", description: "Say hello" },
+    { command: "say_hello", description: "Say hello" },
+    { command: "say_hi", description: "Say hi" },
+]);
+
+// bot.on("message", async (ctx) => {
+//   await ctx.reply("Надо подумать....");
+// });
+
+bot.command(['say_hello', 'hello', 'say_hi'], async (ctx) => {
+    await ctx.reply('Hello, World!');
 })
 
-bot.on('message', async (ctx) => {
-    await ctx.reply('Надо подумать....')       
-})
-
-
-bot.start()
+bot.command("start", async (ctx) => {
+  await ctx.reply("Привет! Я бот.");
+});
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+bot.start();
