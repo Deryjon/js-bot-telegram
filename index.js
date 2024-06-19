2f0f165d5784a3522d7ccb163355137dca245c43
Dery Jon <yusupoviskandar1005@gmail.com>
2024-06-19 17:03:47 +0500

    feat: add moodkeyboard (2024-06-19, 17:03)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Bot, GrammyError, HttpError } = require("grammy");
+const { Bot, GrammyError, HttpError, Keyboard } = require("grammy");
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
@@ -10,6 +10,32 @@ bot.api.setMyCommands([
     { command: "say_hi", description: "Say hi" },
 ]);
 
+
+
+bot.command("start", async (ctx) => {
+  await ctx.reply("Привет! Я бот. Тг канал: <a href='https://t.me/dery_budni'>ссылка</a>", {
+parse_mode: "HTML",
+});
+});
+
+bot.command('mood', async (ctx) => {
+    const moodKeyboard = new Keyboard().text('Well').text('Bad').text('Good').resized().oneTime();
+    await ctx.reply('How are you feeling today?', { reply_markup: moodKeyboard });
+})
+
+// reply_parameters: {
+//     message_id: ctx.msg.message_id,
+// }
+bot.hears('ID', async (ctx) => {
+    await ctx.reply('Ваш ID: ' + ctx.from.id);
+})
+// bot.on('msg').filter((ctx) => {
+//    return ctx.from.id === 783350617
+// }, async (ctx) => {
+    //     await ctx.reply('Привет, админ!')
+    // });
+    
+
 // bot.on("message", async (ctx) => {
 //   await ctx.reply("Надо подумать....");
 // });
@@ -18,9 +44,6 @@ bot.command(['say_hello', 'hello', 'say_hi'], async (ctx) => {
     await ctx.reply('Hello, World!');
 })
 
-bot.command("start", async (ctx) => {
-  await ctx.reply("Привет! Я бот.");
-});
 bot.catch((err) => {
   const ctx = err.ctx;
   console.error(`Error while handling update ${ctx.update.update_id}:`);
